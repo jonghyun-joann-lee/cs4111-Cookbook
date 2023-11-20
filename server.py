98ff3e12c435a1175e234f874f6cbec08a4ab0cd
Jonghyun Lee <joannlee0919@gmail.com>
2023-11-19 19:50:58 -0500

    Added page for each category that list all recipes in the category

--- server.py
+++ server.py
@@ -201,9 +201,30 @@ def index():
 # Notice that the function name is another() rather than index()
 # The functions for each app.route need to have different names
 #
-@app.route('/another')
-def another():
-  return render_template("another.html")
+@app.route('/category/<category_name>')
+def category(category_name):
+  cursor = g.conn.execute(text("""
+                               SELECT R.RecipeName, P.DisplayName, C.CategoryName, R.TotalTime, R.AggregatedRating, R.Calories, R.Sugar
+                               FROM Recipes_written_by R, Categories C, belongs_to B, Authors A, People P
+                               WHERE R.RecipeID = B.RecipeID AND B.CategoryID = C.CategoryID
+                               AND R.UserID = A.UserID AND A.UserID = P.UserID
+                               AND C.CategoryName = :categoryname
+                               """), categoryname=category_name)
+  g.conn.commit()
+  recipes_in_category = [] # a list of tuples 
+  for result in cursor:
+    time = result[3] #R.TotalTime is integer in minutes
+    hours = time // 60
+    mins = time % 60
+    formatted_time = f"{hours} hr {mins} min"
+    result[3] = formatted_time #overwrite TotalTime with the formatted version
+    recipes_in_category.append(tuple(result))
+  cursor.close()
+
+  context = dict(category=category_name, recipes=recipes_in_category)
+  return render_template("category_recipes.html", **context)
+
+
 
 
 # Example of adding new data to the database
